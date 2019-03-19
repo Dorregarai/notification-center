1f5191d8666841a1df09ad8f9c446ebe5e2cc7bc
Zavizion Valentine <sterben1343@gmail.com>
2019-03-19 15:41:16 +0300

    Setted up Webpack, Babel, React

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FormContainer from "./js/components/container/FormContainer.js";
+
+ReactDOM.render(<FormContainer />, document.getElementById("create-article-form"));
